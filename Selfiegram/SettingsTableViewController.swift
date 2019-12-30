b5739fe130d03dc12ab15d8025998126277b6892
DEV3L <jus.beall@gmail.com>
2019-12-30 08:01:53 -0500

    Fix segue from list to settings

---
+++ Selfiegram/SettingsTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsTableViewController.swift
+//  Selfiegram
+//
+//  Created by Justin Beall on 12/29/19.
+//  Copyright © 2019 dev3lsolutions. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
